b77d3f083b8c1ff2849038c97523ebea50efed39
Brandon Rainey <brandonhrainey@gmail.com>
2022-12-04 01:03:25 -0500

    added functionality to category cards

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
 import mensTP from '../public/mens-tp.png'
+import { useRouter } from 'next/router'
 
 type ProductsProps = {
   products: any
 }
 
 export default function Categories({ products }: ProductsProps) {
+  const router = useRouter()
   return (
     <div className="w-full mt-10">
       <p className="text-2xl font-semibold ml-12">Shop Our Top Categories</p>
       <div className="flex flex-wrap lg:flex-nowrap justify-between mt-8 gap-y-3">
         <div className='flex w-full justify-around '>
-          <div className="flex  flex-col  sm:h-72 w-40 sm:w-56 border rounded-lg bg-[#d79715] justify-around items-center ">
+          <div className="flex  flex-col  sm:h-72 w-40 sm:w-56 border rounded-lg bg-[#d79715] hover:bg-[#e7a218] justify-around items-center cursor-pointer " onClick={() => router.push('/mens')}>
             <p className="pb-2  text-white text-lg font-semibold">MENS</p>
             <Image
               src={mensTP}
@@ -22,7 +24,7 @@ export default function Categories({ products }: ProductsProps) {
               className="bg-transparent"
             />
           </div>
-          <div className="flex flex-col  sm:h-72 w-40 sm:w-56 border rounded-lg bg-[#dc3259] justify-around items-center">
+          <div className="flex flex-col  sm:h-72 w-40 sm:w-56 border rounded-lg bg-[#dc3259] hover:bg-[#f33763] justify-around items-center cursor-pointer" onClick={() => router.push('/womens')}>
             <p className="mt-2 text-white text-lg font-semibold">WOMENS</p>
             <Image
               src={'/womens-tp.png'}
@@ -35,7 +37,7 @@ export default function Categories({ products }: ProductsProps) {
         </div>
 
         <div className='flex w-full justify-around'>
-          <div className="flex flex-col  sm:h-72  w-40 sm:w-56 border rounded-lg bg-[#16cbb6] justify-around items-center">
+          <div className="flex flex-col  sm:h-72  w-40 sm:w-56 border rounded-lg bg-[#15baa6] hover:bg-[#16cbb6] justify-around items-center cursor-pointer" onClick={() => router.push('/jewelery')}>
             <p className="mt-2 text-white text-lg font-semibold">JEWERLY</p>
             <Image
               src={'/jewerly-tp.png'}
@@ -45,7 +47,7 @@ export default function Categories({ products }: ProductsProps) {
               className="bg-transparent"
             />
           </div>
-          <div className="flex flex-col  sm:h-72  w-40 sm:w-56 border rounded-lg bg-[#4611b8] justify-around items-center">
+          <div className="flex flex-col  sm:h-72  w-40 sm:w-56 border rounded-lg bg-[#4611b8] hover:bg-[#4f14d0] justify-around items-center cursor-pointer" onClick={() => router.push('/electronics')}>
             <p className="mt-2 text-white text-lg font-semibold">ELECTRONICS</p>
             <Image
               src={'/electronics-tp.png'}
---
+++ pages/[category].tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react'
+import { Router, useRouter } from 'next/router'
+import Header from '../components/Header'
+import Image from 'next/image'
+import { StarIcon } from '@heroicons/react/24/solid'
+import { useDispatch } from 'react-redux'
+import { addToCart } from '../slices/cartSlice'
+
+type ProductsProps = {
+    products: any
+  }
+
+  const MAX_RATING = 5
+  const MIN_RATING = 1
+
+export default function Category({ products }: ProductsProps) {
+    const router = useRouter()
+    const { category } = router.query
+    const [categoryArray, setCategoryArray] = useState([])
+
+    const dispatch = useDispatch()
+
+  function addItemToCart(index: any) {
+    dispatch(addToCart(products[index]))
+  }
+
+    
+    useEffect(() => {
+        let name = ''
+        switch (category) {
+            case 'mens':
+                name = "men's clothing"
+                break;
+            case 'womens':
+                name = "women's clothing"
+                break;
+            case 'jewelery':
+                name = "jewelery"
+                break;
+            case 'electronics':
+                name = "electronics"
+                break;
+        }
+        let array = products.filter((product: any) => product.category == name)
+        setCategoryArray(array)
+    }, [])
+    
+  return (
+    <div>
+        <Header />
+        <p className="text-3xl font-bold pl-6 pt-6 capitalize">{category}</p>
+      <div className="w-full self-center gap-y-4 gap-x-4 px-4 grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {categoryArray.map((product: any, index: any) => (
+          <div className="flex justify-center ">
+            <div className="flex flex-col  w-full h-96 gap-1  pb-1 mt-8 rounded-xl self-center justify-end ">
+              <div className="w-full h-full flex justify-center bg-white">
+                <Image
+                  src={product.image}
+                  height={120}
+                  width={120}
+                  alt="product image"
+                  className="self-center "
+                />
+              </div>
+              <div className="flex flex-col bg-[#f6f6f6] p-1 rounded-lg">
+                <div className="flex w-full  ">
+                  <p className="font-semibold text-medium mr-6">
+                    {product.title}
+                  </p>
+                  <p className="ml-auto text-2xl font-bold">${product.price}</p>
+                </div>
+
+                <p className="text-xs line-clamp-2 ">{product.description}</p>
+                <div className="flex ">
+                  {Array(
+                    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) +
+                      MIN_RATING
+                  )
+                    .fill(undefined)
+                    .map((_, index) => (
+                      <StarIcon className="h-5 text-yellow-300" />
+                    ))}
+                </div>
+
+                <button
+                  className=" w-28 p-1 bg-white rounded-2xl self-center border-2 border-black font-semibold text-sm hover:bg-black hover:text-white"
+                  onClick={() => addItemToCart(index)}
+                >
+                  add to cart
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context: any) {
+    const products = await fetch('https://fakestoreapi.com/products').then(
+      (res) => res.json()
+    )
+
+    
+  
+    return {
+      props: {
+        products,
+      },
+    }
+  }
