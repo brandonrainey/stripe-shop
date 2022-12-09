4efd4b92073beab5e7a71e96a27fbe05b37c8b17
Brandon Rainey <brandonhrainey@gmail.com>
2022-12-09 18:52:53 -0500

    slow search component fix

--- components/SearchDropdown.tsx
+++ components/SearchDropdown.tsx
@@ -11,7 +11,7 @@ export default function SearchDropdown({ filteredProducts, searchInput }: Search
 
   return (
     <div className={`absolute flex flex-col h-auto w-full z-40 bg-white shadow-md border rounded-lg top-[32px]  `}>
-        {filteredProducts?.map((item: any, index: any) => (
+        {filteredProducts.map((item: any, index: any) => (
             <div className='bg-white p-1 truncate rounded-lg hover:bg-slate-200 cursor-pointer text-sm' key={index}>{item.title}</div>
         ))}
         <div className={`${filteredProducts.length === 0 ? '' : 'hidden'} p-1 text-sm`}>No matching results</div>
